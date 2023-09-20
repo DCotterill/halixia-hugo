be343ef7dd351ba11006faee8a94da39f0d0468e
Darren Cotterill <darren.cotterill@gmail.com>
2023-09-20 16:33:11 +1000

    update web pages

---
+++ content/ma/drink-more-coffee-3036.md
@@ -0,0 +1,22 @@
+---
+title: "Drink more coffee"
+date: 2020-09-01T12:49:27+06:00
+featureImage: images/ma/placeholder.jpeg
+layout: static
+---
+
+Enjoy coffee's benefits.
+
+Indulge in moderate coffee consumption for increased alertness, potential antioxidant benefits, and improved mood.
+
+Coffee can boost cognitive function and memory, enhance physical performance and motor skills, and even offer potential protective effects against certain health conditions such as heart disease and cancer.
+
+<a class="ma-link" href="https://www.healthline.com/nutrition/top-evidence-based-health-benefits-of-coffee"><div class="ma-card ma-card-Health"><div class="ma-icon"><img src ="/images/icon-check.png"/></div><div class="ma-name"><p>Healthline</p></div><div class="ma-paid-text"><span>Free </span></div></div></a><a class="ma-link" href="https://www.bbc.co.uk/programmes/m0017kc7"><div class="ma-card ma-card-Health"><div class="ma-icon"><img src ="/images/icon-check.png"/></div><div class="ma-name"><p>BBC</p></div><div class="ma-paid-text"><span>Free</span></div></div></a><a class="ma-link" href="https://www.awin1.com/cread.php?awinmid=20241&awinaffid=1198638&ued=https%3A%2F%2Fwww.realfoodhub.co.uk%2Fsupplier-listing%2Frise-coffee-box"><div class="ma-card ma-card-Health"><div class="ma-icon"><img src ="/images/icon-pound.png"/></div><div class="ma-name"><p>RealFoodHub</p></div><div class="ma-paid-text"><span>Paid</span></div></div></a>  
+
+<br/><br/>
+
+
+
+
+
+
--- test_urls.py
+++ test_urls.py
@@ -7,25 +7,44 @@ count = 0
 # with open('resources/Dummy Data_final_310123 - with full content samples.xlsx - Full Content Samples.csv'
 #         , newline='') as csvfile:
 
-f = open('resources/url-errors.csv', 'a')
+f = open('resources/url-errors-18-09-23.csv', 'a')
+f_whitelist = open('resources/url-whitelist.csv')
+
+whitelist_urls = []
+whitelist_reader = csv.reader(f_whitelist, delimiter=',')
+for whitelist_url in whitelist_reader:
+    whitelist_urls.append(whitelist_url)
 
 writer = csv.writer(f)
 links = []
 
-def test_url(link):
+def is_whitelisted(link):
+    for url in whitelist_urls:
+        # print(url[0])
+        if link.startswith(url[0]):
+            return True
+    return False
+
+def test_url(internal_name, link):
     if link and link not in links:
         links.append(link)
-        try:
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-}
-            request = requests.get(link, timeout=10, headers = headers)
-            if request.status_code != 200:
-                writer.writerow([link, request.status_code])
-        except:
-            writer.writerow([link, "Error"])
+        if is_whitelisted(link):
+            print("Whitelist:" + link)
+        else:
+            # print("Testing:" + link)
+            try:
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+                }
+                request = requests.get(link, timeout=10, headers = headers)
+                if request.status_code != 200:
+                    print("Error:" + link + ":" + request.status_code)
+                    writer.writerow([link, request.status_code])
+            except:
+                print("Error:" + link )
+                writer.writerow([internal_name, link, "Error"])
 
-with open('resources/MA Database 220523.xlsx - Upload Prep -v7.csv'
+with open('resources/MA Database 220523.xlsx - Upload Prep-v11.csv'
         , newline='') as csvfile:
 
     reader = csv.reader(csvfile, delimiter=',')
@@ -48,12 +67,13 @@ with open('resources/MA Database 220523.xlsx - Upload Prep -v7.csv'
         primary_link_3 = row[43]
         primary_link_4 = row[58]
         primary_link_5 = row[73]
-        if count >= 400:
-            test_url(primary_link_1)
-            test_url(primary_link_2)
-            test_url(primary_link_3)
-            test_url(primary_link_4)
-            test_url(primary_link_5)
+        # if count < 500:
+        if count >= 500:
+            test_url(internal_name, primary_link_1)
+            test_url(internal_name, primary_link_2)
+            test_url(internal_name, primary_link_3)
+            test_url(internal_name, primary_link_4)
+            test_url(internal_name, primary_link_5)
 
         count = count + 1
 
